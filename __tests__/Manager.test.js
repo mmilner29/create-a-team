90528d40346a2697768b4a093c3dd15ef5b6cee9
mmilner29 <madisonm4514@gmail.com>
2022-04-03 12:32:12 -0700

    manager test

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,2 +1,9 @@
 const Manager = require('../lib/Manager');
-const Employee = require('../lib/Employee');
+
+
+test('creates a manager object', () => {
+    const manager = new Manager('Madi', 34, 'madi@gmail.com', 355);
+
+    expect(manager.officeNumber).toEqual(expect.any(Number));
+
+});
