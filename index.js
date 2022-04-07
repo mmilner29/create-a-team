109076a98de9459133974e513cfc93c404694794
mmilner29 <madisonm4514@gmail.com>
2022-04-06 18:04:31 -0700

    prompts for adding team members

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const questions = [
     },
     {
         type: 'input',
-        name: 'employee-id',
+        name: 'officeNumber',
         message: 'What is their office number?'
     },   
 ]
@@ -37,7 +37,7 @@ const addMembers = [
 
 const memberType = [
     {
-        type: 'checkbox',
+        type: 'list',
         name: 'member',
         message: 'What type of team member would you like to add?',
         choices: ['Manager', 'Engineer', 'Intern']
@@ -45,6 +45,22 @@ const memberType = [
 ]
 
 const engQuestion = [
+    {
+        type: 'input',
+        name: 'name',
+        message: "What is the their name?"
+
+    },
+    {
+        type: 'input',
+        name: 'employee-id',
+        message: 'What is their employee ID?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is their email address?'
+    },
     {
         type: 'input',
         name: 'github',
@@ -56,30 +72,93 @@ const engQuestion = [
 const intQuestion = [
     {
         type: 'input',
+        name: 'name',
+        message: "What is the their name?"
+
+    },
+    {
+        type: 'input',
+        name: 'employee-id',
+        message: 'What is their employee ID?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is their email address?'
+    },
+    {
+        type: 'input',
         name: 'school',
         message: 'What school do they attend?'
     },
 ]
+// Function for Intern
+function addIntern() {
+    inquirer.prompt(intQuestion)
+    .then((intAnswers) => {
+        console.log(intAnswers);
+        inquirer.prompt(addMembers)
+        .then((response) => {
+            if (!response.new) {
+                console.log('Your team file has been created!');
+            } else {
+                init();
+            }
+        })
+    })
+};
+// Function for Engineer
+function addEngineer() {
+    inquirer.prompt(engQuestion)
+    .then((engAnswers) => {
+        console.log(engAnswers);
+        inquirer.prompt(addMembers)
+        .then((response) => {
+            if (!response.new) {
+                console.log('Your team file has been created!');
+            } else {
+                init();
+            }
+        })
+    })
+};
+
+// Function for Manager
+function addManager() {
+    inquirer.prompt(questions)
+    .then((manAnswers) => {
+        console.log(manAnswers);
+        inquirer.prompt(addMembers)
+        .then((response) => {
+            if (!response.new) {
+                console.log('Your team file has been created!');
+            } else {
+                init();
+            }
+        })
+    })
+};
+
 
 // Function to create index.html file
 
 
 // Function to initialize app
 function init() {
-    inquirer.prompt(questions)
-        .then((answers) => {
-            console.log(answers);
-            inquirer.prompt(addMembers)
-            .then((response) => {
-                if (response.new) {
-                    inquirer.prompt(memberType)
-                    .then((mType) => {
-                        console.log(mType);
-                    })
-                } else {
-                    console.log('Your team file has been created!');
-                }
-            })
+    inquirer.prompt(memberType)
+    .then(({member}) => {
+        if (member === 'Engineer') {
+            // console.log(member)
+            addEngineer();
+        } else if (member == 'Intern') {
+            // console.log(member)
+            addIntern();
+        } else if (member == 'Manager') {
+            // console.log(member)
+            addManager();
+        } else {
+            console.log(member);
+        }
     })
 };
 
